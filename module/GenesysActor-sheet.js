83fa4471363fd2f6164efeb62a767f8ce8ab451e
ceedd <59627835+ceedd@users.noreply.github.com>
2020-05-18 14:24:40 +0200

    Add files via upload

---
+++ module/GenesysActor-sheet.js
@@ -0,0 +1,65 @@
+import {genesysUtil} from "./genesysUtil.js";
+import { dicePopout } from "./dicePopout.js";
+
+
+/**
+ * Extend the basic ActorSheet with some very simple modifications
+ * @extends {ActorSheet}
+ */
+export class GenesysActorSheet extends ActorSheet {
+
+    
+
+
+/*--------------------------------------*/
+/*Saves scroll position of a div        */
+/*Big thanks to  Moon Man on the discord*/
+async _render(force = false, options = {}) {
+    this._saveScrollPos(); // Save scroll positions
+    await super._render(force, options);
+    this._setScrollPos();  // Set scroll positions
+}
+
+_saveScrollPos()
+    {
+      if (this.form === null)
+        return;
+
+      const html = $(this.form).parent();
+      this.scrollPos = [];
+      let lists = $(html.find(".sheet-body"));
+      for (let list of lists)
+      {
+        this.scrollPos.push($(list).scrollTop());
+      }
+    }
+
+    _setScrollPos()
+    {
+      if (this.scrollPos)
+      {
+        const html = $(this.form).parent();
+        let lists = $(html.find(".sheet-body"));
+        for (let i = 0; i < lists.length; i++)
+        {
+          $(lists[i]).scrollTop(this.scrollPos[i]);
+        }
+      }
+    }
+
+
+
+
+
+
+
+
+
+
+/** @override */
+_updateObject(event, formData) {
+
+    return this.object.update(formData);
+}
+
+}
--- module/simple.js
+++ module/simple.js
@@ -7,14 +7,15 @@
 // Import Modules
 import { SimpleActor } from "./actor.js";
 import { SimpleItemSheet } from "./item-sheet.js";
-import { SimpleActorSheet } from "./actor-sheet.js";
+import { CharacterActorSheet } from "./actor-sheet.js";
+import { VehicleActorSheet } from "./vehicle-sheet.js"
 
 /* -------------------------------------------- */
 /*  Foundry VTT Initialization                  */
 /* -------------------------------------------- */
 
 Hooks.once("init", async function() {
-  console.log(`Initializing Simple Worldbuilding System`);
+  console.log(`Initializing Genesys`);
 
 	/**
 	 * Set an initiative formula for the system
@@ -30,12 +31,13 @@ Hooks.once("init", async function() {
 
   // Register sheet application classes
   Actors.unregisterSheet("core", ActorSheet);
-  Actors.registerSheet("dnd5e", SimpleActorSheet, { makeDefault: true });
+  Actors.registerSheet("Genesys", CharacterActorSheet,  {types: ["character"], makeDefault: true });
+  Actors.registerSheet("Genesys", VehicleActorSheet,  {types: ["vehicle"], makeDefault: true });
   Items.unregisterSheet("core", ItemSheet);
-  Items.registerSheet("dnd5e", SimpleItemSheet, {makeDefault: true});
+  Items.registerSheet("genesysItems", SimpleItemSheet, {makeDefault: true});
 
   // Register system settings
-  game.settings.register("worldbuilding", "macroShorthand", {
+  game.settings.register("genesys", "macroShorthand", {
     name: "Shortened Macro Syntax",
     hint: "Enable a shortened macro syntax which allows referencing attributes directly, for example @str instead of @attributes.str.value. Disable this setting if you need the ability to reference the full attribute model, for example @attributes.str.label.",
     scope: "world",
